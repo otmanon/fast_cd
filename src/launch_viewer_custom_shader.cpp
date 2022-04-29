219eaf1799fc1bddbfbe928ac9fde6d028da3287
otmanon <otman.benchekroun@mail.mcgill.ca>
2022-04-29 01:11:06 -0400

    changed default custom shader, instead of destroying the shader, and creating a new one each time we want the deformation texture

--- src/launch_viewer_custom_shader.cpp
+++ src/launch_viewer_custom_shader.cpp
@@ -9,145 +9,111 @@ void launch_viewer_custom_shader(igl::opengl::glfw::Viewer& v,
 
     //v.launch_init(true, false);
 
-    std::string mesh_vertex_shader_string =
-        R"(#version 150
-      uniform mat4 view;
-      uniform mat4 proj;
-      uniform mat4 normal_matrix;
-      in vec3 position;
-      in vec3 normal;
-      out vec3 position_eye;
-      out vec3 normal_eye;
-      in vec4 Ka;
-      in vec4 Kd;
-      in vec4 Ks;
-     // in vec2 texcoord;
-      out vec2 texcoordi;
-      out vec4 Kai;
-      out vec4 Kdi;
-      out vec4 Ksi;
+            ///////////////////////////////////////////////////////////////////
+            // Compile new shaders
+            ///////////////////////////////////////////////////////////////////
+   
+        std::string mesh_vertex_shader_string =
+            R"(#version 150
+        uniform mat4 view;
+        uniform mat4 proj;
+        in vec3 position;
+        in vec3 normal;
+        out vec3 position_eye;
+        out vec3 normal_eye;
+        in vec4 Ka;
+        in vec4 Kd;
+        in vec4 Ks;
+        in vec2 texcoord;
+        out vec2 texcoordi;
+        out vec4 Kai;
+        out vec4 Kdi;
+        out vec4 Ksi;
+        
+       uniform int proj_gpu;
+        in float id;
+        uniform int n;
+        uniform int m;
+        uniform int s;
+        uniform float q[512];
+        uniform sampler2D tex;
 
-     in float id;
-     uniform int n;
-     uniform int m;
-     uniform int s;
-     uniform float q[512];
-    uniform sampler2D tex;
+        void main()
+        {
+          vec3 deformed = position;
+          if(proj_gpu == 1)                  //then we should deform our mesh
+          {
+             vec3 displacement = vec3(0,0,0);
+             for(int j = 0;j < m; j++)
+             {
+                  int index = int(id)*m+j;
+                  int si = index % s;
+                  int sj = int((index - si)/s);
+                  displacement = displacement + texelFetch(tex,ivec2(si,sj),0).xyz*q[j];
+             }
+             deformed = position + displacement;
+         }
+          position_eye = vec3 (view * vec4 (deformed, 1.0));
+          gl_Position = proj * vec4 (position_eye, 1.0);
+          Kai = Ka;
+          Kdi = Kd;
+          Ksi = Ks;
+          texcoordi = texcoord;
+        })";
 
-    void main()
-    {
-       vec3 displacement = vec3(0,0,0);
-        int index = int(id)*m + 0;
-        int si = index%s;
-        int sj = int((index-si)/s);
-         displacement = displacement + texelFetch(tex,ivec2(si,sj),0).xyz*q[0]; //
-       // for(int j = 0;j < m; j++)
-       // {
-       //   int index = int(id)*m+j;
-       //   int si = index % s;
-       //   int sj = int((index - si)/s);
-       //   displacement = displacement + texelFetch(tex,ivec2(si,sj),0).xyz*q[j];
-       // }
-      vec3 deformed = position + displacement ; 
+        std::string mesh_fragment_shader_string =
+            R"(#version 150
+          uniform mat4 view;
+          uniform mat4 proj;
+          uniform vec4 fixed_color;
+          in vec3 position_eye;
+          uniform vec3 light_position_eye;
+          vec3 Ls = vec3 (1, 1, 1);
+          vec3 Ld = vec3 (1, 1, 1);
+          vec3 La = vec3 (1, 1, 1);
+          in vec4 Ksi;
+          in vec4 Kdi;
+          in vec4 Kai;
+          in vec2 texcoordi;
+          uniform sampler2D tex;
+          uniform float specular_exponent;
+          uniform float lighting_factor;
+          uniform float texture_factor;
+          uniform float matcap_factor;
+          uniform float double_sided;
+          out vec4 outColor;
+          void main()
+          {
+            vec3 xTangent = dFdx(position_eye);
+            vec3 yTangent = dFdy(position_eye);
+            vec3 normal_eye = normalize( cross( yTangent, xTangent ) );
 
-      position_eye =  vec3 (view * vec4 (deformed, 1.0));
-      gl_Position = proj * vec4 (position_eye, 1.0);
-      normal_eye = vec3 (normal_matrix * vec4 (normal, 0.0));
-      normal_eye = normalize(normal_eye);
-      Kai = Ka;
-      Kdi = Kd;
-      Ksi = Ks;
-    })";
+            if(matcap_factor == 1.0f)
+            {
+              vec2 uv = normalize(normal_eye).xy * 0.5 + 0.5;
+              outColor = texture(tex, uv);
+            }else
+            {
+              vec3 Ia = La * vec3(Kai);    // ambient intensity
 
-    /*
-    std::string mesh_vertex_shader_string =
-        R"(#version 150
-  uniform mat4 view;
-  uniform mat4 proj;
-  uniform mat4 normal_matrix;
-  in vec3 position;
-  in vec3 normal;
-  out vec3 position_eye;
-  out vec3 normal_eye;
-  in vec4 Ka;
-  in vec4 Kd;
-  in vec4 Ks;
-  in vec2 texcoord;
-  out vec2 texcoordi;
-  out vec4 Kai;
-  out vec4 Kdi;
-  out vec4 Ksi;
+              vec3 vector_to_light_eye = light_position_eye - position_eye;
+              vec3 direction_to_light_eye = normalize (vector_to_light_eye);
+              float dot_prod = dot (direction_to_light_eye, normalize(normal_eye));
+              float clamped_dot_prod = abs(max (dot_prod, -double_sided));
+              vec3 Id = Ld * vec3(Kdi) * clamped_dot_prod;    // Diffuse intensity
 
-  void main()
-  {
-    position_eye = vec3 (view * vec4 (position, 1.0));
-    normal_eye = vec3 (normal_matrix * vec4 (normal, 0.0));
-    normal_eye = normalize(normal_eye);
-    gl_Position = proj * vec4 (position_eye, 1.0); //proj * view * vec4(position, 1.0);"
-    Kai = Ka;
-    Kdi = Kd;
-    Ksi = Ks;
-    texcoordi = texcoord;
-  }
-)";*/
-
-    std::string mesh_fragment_shader_string =
-        R"(#version 150
-  uniform mat4 view;
-  uniform mat4 proj;
-  uniform vec4 fixed_color;
-  in vec3 position_eye;
-  in vec3 normal_eye;
-  uniform vec3 light_position_eye;
-  vec3 Ls = vec3 (1, 1, 1);
-  vec3 Ld = vec3 (1, 1, 1);
-  vec3 La = vec3 (1, 1, 1);
-  in vec4 Ksi;
-  in vec4 Kdi;
-  in vec4 Kai;
-  in vec2 texcoordi;
-  uniform sampler2D tex;
-  uniform float specular_exponent;
-  uniform float lighting_factor;
-  uniform float texture_factor;
-  uniform float matcap_factor;
-  uniform float double_sided;
-  out vec4 outColor;
-  void main()
-  {
-
-    vec3 xTangent = dFdx( position_eye );
-    vec3 yTangent = dFdy( position_eye );
-    vec3 faceNormal = normalize( cross( xTangent, yTangent ) );
-    if(matcap_factor == 1.0f)
-    {
-      vec2 uv = normalize(faceNormal).xy * 0.5 + 0.5;
-      outColor = texture(tex, uv);
-    }else
-    {
-      vec3 Ia = La * vec3(Kai);    // ambient intensity
-
-      vec3 vector_to_light_eye = light_position_eye - position_eye;
-      vec3 direction_to_light_eye = normalize (vector_to_light_eye);
-      float dot_prod = dot (direction_to_light_eye, normalize(faceNormal));
-      float clamped_dot_prod = abs(max (dot_prod, -double_sided));
-      vec3 Id = Ld * vec3(Kdi) * clamped_dot_prod;    // Diffuse intensity
-
-      vec3 reflection_eye = reflect (-direction_to_light_eye, normalize(faceNormal));
-      vec3 surface_to_viewer_eye = normalize (-position_eye);
-      float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
-      dot_prod_specular = float(abs(dot_prod)==dot_prod) * abs(max (dot_prod_specular, -double_sided));
-      float specular_factor = pow (dot_prod_specular, specular_exponent);
-      vec3 Is = Ls * vec3(Ksi) * specular_factor;    // specular intensity
-      vec4 color = vec4(lighting_factor * (Is + Id) + Ia + (1.0-lighting_factor) * vec3(Kdi),(Kai.a+Ksi.a+Kdi.a)/3);
-      outColor = mix(vec4(1,1,1,1), texture(tex, texcoordi), texture_factor) * color;
-      
-      if (fixed_color != vec4(0.0)) outColor = fixed_color;
-     
-  //  outColor = vec4(0,1, 1, 1);
-    }
-  }
-)";
+              vec3 reflection_eye = reflect (-direction_to_light_eye, normalize(normal_eye));
+              vec3 surface_to_viewer_eye = normalize (-position_eye);
+              float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
+              dot_prod_specular = float(abs(dot_prod)==dot_prod) * abs(max (dot_prod_specular, -double_sided));
+              float specular_factor = pow (dot_prod_specular, specular_exponent);
+              vec3 Is = Ls * vec3(Ksi) * specular_factor;    // specular intensity
+              vec4 color = vec4(lighting_factor * (Is + Id) + Ia + (1.0-lighting_factor) * vec3(Kdi),(Kai.a+Ksi.a+Kdi.a)/3);
+              outColor = mix(vec4(1,1,1,1), texture(tex, texcoordi), texture_factor) * color;
+              if (fixed_color != vec4(0.0)) outColor = fixed_color;
+            }
+          }
+        )";
 
     std::string overlay_vertex_shader_string =
         R"(#version 150
--- src/user_interface.cpp
+++ src/user_interface.cpp
@@ -30,7 +30,7 @@ bool InteractiveCDHook::render(igl::opengl::glfw::Viewer& viewer)
     Eigen::VectorXf z = 0*z_curr.cast<float>();
     z(0) = 0.1;
 
-   if (as.proj_gpu == 1)
+   if (as.proj_gpu == 1) //render with GPU
    {
         GLuint prog_id = viewer.data().meshgl.shader_mesh;
         glActiveTexture(GL_TEXTURE0);
@@ -98,6 +98,7 @@ bool InteractiveCDHook::render(igl::opengl::glfw::Viewer& viewer)
 
 void InteractiveCDHook::init_viewer(igl::opengl::glfw::Viewer& v)
 {
+    init_vis_state();
     using namespace Eigen;
     using namespace std;
     Eigen::MatrixXd V = this->V;
@@ -136,131 +137,14 @@ void InteractiveCDHook::init_viewer(igl::opengl::glfw::Viewer& v)
     // Initialize viewer and opengl context
     ///////////////////////////////////////////////////////////////////
    // igl::opengl::glfw::Viewer v;
-    v.data().set_mesh(V, F);
+    v.data().set_mesh(V0, F);
     v.data().invert_normals = false;
     v.data().double_sided = true;
     v.data().set_face_based(false);
     v.data().show_lines = false;
-    //v.launch_init(true, false);
-    // v.data().meshgl.init();
-    //igl::opengl::destroy_shader_program(v.data().meshgl.shader_mesh);
-    
-
-            ///////////////////////////////////////////////////////////////////
-            // Compile new shaders
-            ///////////////////////////////////////////////////////////////////
-    {
-        std::string mesh_vertex_shader_string =
-            R"(#version 150
-        uniform mat4 view;
-        uniform mat4 proj;
-        in vec3 position;
-        in vec3 normal;
-        out vec3 position_eye;
-        out vec3 normal_eye;
-        in vec4 Ka;
-        in vec4 Kd;
-        in vec4 Ks;
-        in vec2 texcoord;
-        out vec2 texcoordi;
-        out vec4 Kai;
-        out vec4 Kdi;
-        out vec4 Ksi;
-        
-       uniform int proj_gpu;
-        in float id;
-        uniform int n;
-        uniform int m;
-        uniform int s;
-        uniform float q[512];
-        uniform sampler2D tex;
-
-        void main()
-        {
-          vec3 deformed = position;
-          if(proj_gpu == 1)                  //then we should deform our mesh
-          {
-             vec3 displacement = vec3(0,0,0);
-             for(int j = 0;j < m; j++)
-             {
-                  int index = int(id)*m+j;
-                  int si = index % s;
-                  int sj = int((index - si)/s);
-                  displacement = displacement + texelFetch(tex,ivec2(si,sj),0).xyz*q[j];
-             }
-             deformed = position + displacement;
-         }
-          position_eye = vec3 (view * vec4 (deformed, 1.0));
-          gl_Position = proj * vec4 (position_eye, 1.0);
-          Kai = Ka;
-          Kdi = Kd;
-          Ksi = Ks;
-          texcoordi = texcoord;
-        })";
-
-                std::string mesh_fragment_shader_string =
-                    R"(#version 150
-          uniform mat4 view;
-          uniform mat4 proj;
-          uniform vec4 fixed_color;
-          in vec3 position_eye;
-          uniform vec3 light_position_eye;
-          vec3 Ls = vec3 (1, 1, 1);
-          vec3 Ld = vec3 (1, 1, 1);
-          vec3 La = vec3 (1, 1, 1);
-          in vec4 Ksi;
-          in vec4 Kdi;
-          in vec4 Kai;
-          in vec2 texcoordi;
-          uniform sampler2D tex;
-          uniform float specular_exponent;
-          uniform float lighting_factor;
-          uniform float texture_factor;
-          uniform float matcap_factor;
-          uniform float double_sided;
-          out vec4 outColor;
-          void main()
-          {
-            vec3 xTangent = dFdx(position_eye);
-            vec3 yTangent = dFdy(position_eye);
-            vec3 normal_eye = normalize( cross( yTangent, xTangent ) );
-
-            if(matcap_factor == 1.0f)
-            {
-              vec2 uv = normalize(normal_eye).xy * 0.5 + 0.5;
-              outColor = texture(tex, uv);
-            }else
-            {
-              vec3 Ia = La * vec3(Kai);    // ambient intensity
-
-              vec3 vector_to_light_eye = light_position_eye - position_eye;
-              vec3 direction_to_light_eye = normalize (vector_to_light_eye);
-              float dot_prod = dot (direction_to_light_eye, normalize(normal_eye));
-              float clamped_dot_prod = abs(max (dot_prod, -double_sided));
-              vec3 Id = Ld * vec3(Kdi) * clamped_dot_prod;    // Diffuse intensity
-
-              vec3 reflection_eye = reflect (-direction_to_light_eye, normalize(normal_eye));
-              vec3 surface_to_viewer_eye = normalize (-position_eye);
-              float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
-              dot_prod_specular = float(abs(dot_prod)==dot_prod) * abs(max (dot_prod_specular, -double_sided));
-              float specular_factor = pow (dot_prod_specular, specular_exponent);
-              vec3 Is = Ls * vec3(Ksi) * specular_factor;    // specular intensity
-              vec4 color = vec4(lighting_factor * (Is + Id) + Ia + (1.0-lighting_factor) * vec3(Kdi),(Kai.a+Ksi.a+Kdi.a)/3);
-              outColor = mix(vec4(1,1,1,1), texture(tex, texcoordi), texture_factor) * color;
-              if (fixed_color != vec4(0.0)) outColor = fixed_color;
-            }
-          }
-        )";
-
-        igl::opengl::create_shader_program(
-            mesh_vertex_shader_string,
-            mesh_fragment_shader_string,
-            {},
-            v.data().meshgl.shader_mesh);
-    }
-
+     
     ///////////////////////////////////////////////////////////////////
-   // Send texture and vertex attributes to GPU
+   // Send texture and vertex attributes to GPU... should make this its own function but maybe it's okay
    ///////////////////////////////////////////////////////////////////
       {
       GLuint prog_id = v.data().meshgl.shader_mesh;
@@ -294,52 +178,7 @@ void InteractiveCDHook::init_viewer(igl::opengl::glfw::Viewer& v)
       q0(0) = 0.01;
       Eigen::VectorXf q1 = Eigen::VectorXf::Zero(m, 1);
 
-      //v.callback_pre_draw = [&](igl::opengl::glfw::Viewer& v) ->bool
-      //{
-      //    static size_t count = 0;
-      //    const int keyrate = 15;
-      //    if (count % keyrate == 0)
-      //    {
-      //       // q0 = q1;
-      //        q1 = Eigen::VectorXf::Random(m, 1).array() * 0.5 + 0.0001*0.5;
-      //        q1 = q1.array().pow(100.0).eval();
-      //    }
-      //    Eigen::VectorXf qa = q0 + double(count % keyrate) / (keyrate - 1.0) * (q1 - q0);
-      //    //qa /= qa.sum();
-      //    count++;
-      //    /////////////////////////////////////////////////////////
-      //    // Send uniforms to shader
-      //    /////////////////////////////////////////////////////////
-      //    GLuint prog_id = v.data().meshgl.shader_mesh;
-      //    glUseProgram(prog_id);
-      //    GLint n_loc = glGetUniformLocation(prog_id, "n");
-      //    glUniform1i(n_loc, n);
-      //    GLint m_loc = glGetUniformLocation(prog_id, "m");
-      //    glUniform1i(m_loc, m);
-      //    GLint s_loc = glGetUniformLocation(prog_id, "s");
-      //    glUniform1i(s_loc, s);
-      //    GLint q_loc = glGetUniformLocation(prog_id, "q");
-      //    glUniform1fv(q_loc, U.cols(), q0.data());
-      //
-      //
-      //    // Do this now so that we can stop texture from being loaded by viewer
-      //    if (v.data().dirty)
-      //    {
-      //        v.data().updateGL(
-      //            v.data(),
-      //            v.data().invert_normals,
-      //            v.data().meshgl
-      //        );
-      //        v.data().dirty = igl::opengl::MeshGL::DIRTY_NONE;
-      //    }
-      //    v.data().meshgl.dirty &= ~igl::opengl::MeshGL::DIRTY_TEXTURE;
-      //    return false;
-      //};
-
-      v.core().animation_max_fps = 500.0;
-      v.core().is_animating = true;
-    //  v.launch_rendering(true);
-    //  v.launch_shut();
+      //This is very important... otherwise when the new mesh is set, it triggers a dirty call , and igl overwrites the texture with junk...
       v.data().dirty &= ~igl::opengl::MeshGL::DIRTY_TEXTURE;
 }
 
