de47d0790b3d2781438d72cae57f7d2d0d8d2239
otmanon <otman.benchekroun@mail.utoronto.ca>
2022-11-06 01:09:57 -0500

    added weight space clustering

--- include/compute_clusters_igl.h
+++ include/compute_clusters_igl.h
@@ -2,3 +2,7 @@
 #include <Eigen/Core>
 
 void compute_clusters_igl(const Eigen::MatrixXi& T, const Eigen::MatrixXd& B, const Eigen::VectorXd& L, const int num_clusters, const int num_feature_modes, Eigen::VectorXi& labels, Eigen::MatrixXd& C);
+
+
+
+void compute_clusters_weight_space(const Eigen::MatrixXi& T, const Eigen::MatrixXd& W, const Eigen::VectorXd& L, const int num_clusters, const int num_feature_modes, Eigen::VectorXi& labels, Eigen::MatrixXd& C);
